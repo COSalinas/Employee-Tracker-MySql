fb386c2652f1574cfbbff662e0ea23ff1a3e7495
COSalinas <omarsalinas96@hotmail.com>
2020-08-09 16:43:58 -0500

    final commit

--- db/schema.sql
+++ db/schema.sql
@@ -1,35 +1,34 @@
+DROP DATABASE IF EXISTS employee_db;
+
 CREATE DATABASE employee_db;
 
 USE employee_db;
 
+----- Create Department table -----
 CREATE TABLE department (
-    id INT AUTO_INCREMENT, 
-    name VARCHAR(30) NOT NULL, 
-    PRIMARY KEY(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
+----- Create Role Table -----
 CREATE TABLE role (
-    id INT AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL, 
-    salary DECIMAL(6,2) NOT NULL, 
-    department_id INT NOT NULL, 
-    PRIMARY KEY(id),
-    FOREIGN KEY(department_id)
-    REFERENCES department(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
+----- Create Employee Table -----
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT, 
-    first_name VARCHAR(30) NOT NULL, 
-    last_name VARCHAR(30) NOT NULL, 
-    role_id INT NOT NULL, 
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY(id),
-
-    FOREIGN KEY(role_id)
-    REFERENCES role(id),
-
-    FOREIGN KEY(manager_id)
-    REFERENCES employee(id)
-
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,76 @@
 USE employee_db;
 
-INSERT INTO department (name) VALUES ("Human Resources");
-INSERT INTO department (name) VALUES ("Marketing");
-INSERT INTO department (name) VALUES ("Information Technology");
-INSERT INTO department (name) VALUES ("Corporate");
+----- Department Seeds -----
 
-INSERT INTO role (title, salary, department_id) VALUES ("Analyst", 70, 3);
-INSERT INTO role (title, salary, department_id) VALUES ("Communications Associate", 50, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Social Media Manager", 50, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Director", 100, 1);
-INSERT INTO role (title, salary, department_id) VALUES ("Director", 100, 4);
+INSERT INTO department (id, name)
+VALUES (1, "Retail");
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("David", "Jester", 2);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Abby", "Whiteman", 1);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Katie", "Pieto", 3);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Rachael", "Squirm", 4);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Lizzie", "Halep", 5);
+INSERT INTO department (id, name)
+VALUES (2, "Brand Experience");
+
+INSERT INTO department (id, name)
+VALUES (3, "Human Resouces");
+
+INSERT INTO department (id, name)
+VALUES (4, "IT");
+
+----- Role Seeds -----
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Stylist", 42000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (2, "Assistant Store Manager", 60000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (3, "Store Manager", 75000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (4, "Brand Manager", 65000, 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (5, "Business Partner", 70000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (6, "HR Director", 100000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (7, "System Adminstrator", 80000, 4);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (8, "IT Coordinator", 52000, 4);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (9, "IT Manager", 105000, 4);
+
+----- Employees Seeds -----
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (4, "Charbelle", "Chaz", 3, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (5, "Sofi", "Hernan", 4, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (7, "Char", "Dant", 6, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (10, "Jonathan", "Uong", 9, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (3, "Melissa", "Arm", 2, 4);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Alla", "Arous", 1, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (2, "Rosie", "Cole", 1, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (6, "Lianne", "Soon", 5, 7);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (8, "Zach", "Soar", 7, 10);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (9, "Martin", "Cast", 8, 10);
